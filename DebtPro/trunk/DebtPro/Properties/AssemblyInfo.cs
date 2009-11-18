d604353a79a5495f0f6403899c6035bd742a706a
ivanoff.nikolay <ivanoff.nikolay@gmail.com>
2009-11-18 22:28:33 +0000

    Lazy-load in settings Disable sql logging Start version 2.1.2.0

--- DebtPro/trunk/DebtPro/Properties/AssemblyInfo.cs
+++ DebtPro/trunk/DebtPro/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("ru-RU")]
